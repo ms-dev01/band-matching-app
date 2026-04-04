2a80730cedd99e532607a0e29792e2c00e3de13d
you <webmg71@gmail.com>
2026-04-04 23:18:45 +0900

    ホーム画面の作成 Close #8

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # root = 「/」のこと, 特に指定がなければhome#indexを表示する
+  root to: "recruitments#index"
 end
