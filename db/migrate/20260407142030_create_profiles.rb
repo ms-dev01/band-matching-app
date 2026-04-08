325f3cdcc73de7e2e2edf224e27a0eddd0f18f1b
you <webmg71@gmail.com>
2026-04-08 21:21:29 +0900

    Profileモデルとprofilesテーブルの作成

---
+++ db/migrate/20260407142030_create_profiles.rb
@@ -0,0 +1,33 @@
+class CreateProfiles < ActiveRecord::Migration[8.1]
+  def change
+    create_table :profiles do |t|
+      # t.references = このテーブルに外部キーを追加する, :user = 対象のモデル, user_idがnullだと保存しない, user_idが存在しないidだと保存しない
+      t.references :user, null: false, foreign_key: true
+      # ニックネーム
+      t.string :nickname, null: false
+      # 性別
+      t.integer :gender
+      # 生年月日
+      t.date :birth_date
+      # 担当パート
+      t.integer :part
+      # 担当年数
+      t.integer :experience_year
+      # 担当月数
+      t.integer :experience_month
+      # 活動志向
+      t.integer :activity_style
+      # 練習スタイル
+      t.integer :practice_style
+      # 楽曲タイプ
+      t.integer :music_type
+      # ライブ出演希望
+      t.boolean :wants_live_performance, default: false
+      # SNSリンク
+      t.text :sns_links
+      # 自己紹介文
+      t.text :bio
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_22_065251) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_07_142030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "activity_style"
+    t.text "bio"
+    t.date "birth_date"
+    t.datetime "created_at", null: false
+    t.integer "experience_month"
+    t.integer "experience_year"
+    t.integer "gender"
+    t.integer "music_type"
+    t.string "nickname", null: false
+    t.integer "part"
+    t.integer "practice_style"
+    t.text "sns_links"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.boolean "wants_live_performance", default: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,4 +44,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_22_065251) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "profiles", "users"
 end
