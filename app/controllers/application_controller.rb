f86d04d7fb81b613e54cb01d74d6ef4e4b3bf7e7
you <webmg71@gmail.com>
2026-04-18 21:29:22 +0900

    ログイン後にプロフィールが未完成であればプロフィール編集画面へリダイレクトする Refs #18

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,21 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  # ログイン後、プロフィールの必須項目が入力されていなければ、プロフィール編集画面に遷移
+  before_action :ensure_profile!
+
+  # ログイン後、プロフィールの必須項目が入力されていなければ、プロフィール編集画面に遷移
+  def ensure_profile!
+    # ログインしていない場合、何もせず終了
+    return unless user_signed_in?
+
+    # プロフィール画面の場合、何もせず終了
+    return if controller_name == "profiles"
+
+    # プロフィールの必須項目が入力されていなければ、プロフィール編集画面に遷移
+    unless current_user.profile.completed?
+      redirect_to edit_profile_path
+    end
+  end
 end
