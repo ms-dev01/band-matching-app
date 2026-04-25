5f0e62f18f06c74abcd2001aef0911b3220435c8
you <webmg71@gmail.com>
2026-04-25 23:32:03 +0900

    同一活動ジャンルの重複登録を防ぐバリデーションを追加 Refs #20

--- app/models/profile_activity_genre.rb
+++ app/models/profile_activity_genre.rb
@@ -1,4 +1,7 @@
 class ProfileActivityGenre < ApplicationRecord
   belongs_to :profile
   belongs_to :activity_genre
+
+  # バリデーション設定（同じ活動ジャンルの重複登録を防止）
+  validates :profile_id, uniqueness: { scope: :activity_genre_id }
 end
