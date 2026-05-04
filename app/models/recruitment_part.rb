9696f41c3957e52477f9aba9a1790a2b454e106a
you <webmg71@gmail.com>
2026-05-05 02:00:02 +0900

    募集_パート（recruitment_parts）テーブル作成 Refs #64

---
+++ app/models/recruitment_part.rb
@@ -0,0 +1,12 @@
+class RecruitmentPart < ApplicationRecord
+  belongs_to :band_recruitment
+
+  # 募集パートのパターンを定義
+  enum :part, { vocal: 0, guitar: 1, bass: 2, drums: 3, keyboard: 4 }
+
+  # バリデーション設定
+  validates :part, presence: true
+  validates :max_count, presence: true
+  # 同じパートの重複登録を防止
+  validates :part, uniqueness: { scope: :band_recruitment_id }
+end
