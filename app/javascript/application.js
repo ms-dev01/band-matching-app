e8f4261ad6e1b79cf01ee805a76b66f938e127bb
you <webmg71@gmail.com>
2026-06-20 14:16:35 +0900

    javaScriptの実装（参加希望ボタンクリックで、コメント入力フォームと送信/キャンセルボタンを表示 / キャンセルボタンクリックで、コメント入力フォームを閉じる）

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+// jqueryの読み込み
+import $ from "jquery";
+
+document.addEventListener("turbo:load", () => {
+  $(".js-application-btn").on("click", () => {
+    $(".js-application-container").css("display", "block");
+    $(".btn-container").css("display", "none");
+  });
+
+  $(".js-cancel-btn").on("click", () => {
+    $(".js-application-container").css("display", "none");
+    $(".btn-container").css("display", "block");
+  });
+});
