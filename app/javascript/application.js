0bca9e8dc3b7958b369a74d697bc90dd3dbf40a1
you <webmg71@gmail.com>
2026-07-12 22:00:22 +0900

    募集詳細画面のアイコンクリックでプロフィールのモーダルを表示 Refs #100

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,17 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
+// controllers/index.jsの読み込み
 import "controllers";
 // jqueryの読み込み
 import $ from "jquery";
-
-document.addEventListener("turbo:load", () => {
-  $(".js-application-btn").on("click", () => {
-    $(".js-application-container").css("display", "block");
-    $(".btn-container").css("display", "none");
-  });
-
-  $(".js-cancel-btn").on("click", () => {
-    $(".js-application-container").css("display", "none");
-    $(".btn-container").css("display", "block");
-  });
-});
