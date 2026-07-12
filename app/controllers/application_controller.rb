17e80db19b90ba3e326c8081982518220117d5f0
you <webmg71@gmail.com>
2026-07-12 22:04:13 +0900

    ensure_profileメソッドの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,23 +5,23 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
-  # ログイン後、プロフィールの必須項目が入力されていなければ、プロフィール編集画面に遷移
+  # ログイン後、プロフィールの必須項目が入力されていなければ、プロフィール作成画面に遷移
   before_action :ensure_profile!
 
-  # ログイン後、プロフィールの必須項目が入力されていなければ、プロフィール編集画面に遷移
+  # ログイン後、プロフィールの必須項目が入力されていなければ、プロフィール作成画面に遷移
   def ensure_profile!
     # ログインしていない場合、何もせず終了
     return unless user_signed_in?
 
-    # プロフィール編集・更新処理は対象外
-    return if controller_name == "profiles" && action_name.in?(%w[edit update])
+    # プロフィール作成・編集・更新処理は対象外
+    return if controller_name == "profiles" && action_name.in?(%w[new create edit update])
 
     # Devise認証関連の画面は対象外
     return if devise_controller?
 
-    # プロフィールの必須項目が入力されていなければ、プロフィール編集画面に遷移
-    unless current_user.profile&.completed?
-      redirect_to edit_profile_path
+    # プロフィールの必須項目が入力されていなければ、プロフィール作成画面に遷移
+    if current_user.profile.nil?
+      redirect_to new_profile_path
     end
   end
 end
